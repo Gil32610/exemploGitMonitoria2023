57c45b63bdcf9a4cfa0704b6c2048e29c299e913
GIL ARAUJO <gil.00000032610@unicap.br>
2023-09-14 13:37:29 -0300

    printing messages

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("-".repeat(20));
+
         System.out.println("Hello, World!");
     }
 }
